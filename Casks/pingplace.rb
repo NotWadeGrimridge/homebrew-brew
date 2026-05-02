0748ba0242e9a523f7a3b1ffdd85b276a7f2e930
NotWadeGrimridge <wade@grimridge.net>
2026-05-02 15:09:46 +0530

    pingplace: v1.4.0
    Signed-off-by: NotWadeGrimridge <wade@grimridge.net>

--- Casks/pingplace.rb
+++ Casks/pingplace.rb
@@ -1,13 +1,13 @@
 cask "pingplace" do
-  version "1.3.1"
-  sha256 "eed3e062c1ffd2fae224e414a5e5d068daab76aef2bcf7ab9f142ec20da07de7"
+  version "1.4.0"
+  sha256 "0b88ecca4462570e47bd3f4c77df4c58d2feffa23c5974b166eb5128f12716d8"
 
   url "https://github.com/NotWadeGrimridge/PingPlace/releases/download/v#{version}/PingPlace.app.tar.gz"
   name "PingPlace"
   desc "Control where notifications appear on your screen."
   homepage "https://github.com/NotWadeGrimridge/PingPlace"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :tahoe"
 
   app "PingPlace.app"
 
@@ -15,12 +15,4 @@ cask "pingplace" do
     "~/Library/LaunchAgents/com.grimridge.PingPlace.plist",
     "~/Library/Preferences/com.grimridge.PingPlace.plist",
   ]
-
-  caveats <<~EOS
-    If you're upgrading from version 1.0 to 1.1, please remove PingPlace from
-    System Settings > Privacy & Security > Accessibility when prompted and
-    add it again. You might also have to "trust" the app.
-
-    Sorry for the inconvenience (blame Apple), it shouldn't happen again!
-  EOS
 end
