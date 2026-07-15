6177ec8a3004b1e4ba7a88b3361bc11840a5060f
NotWadeGrimridge <wade@grimridge.net>
2026-07-16 02:39:38 +0530

    pingplace: v1.4.2
    Signed-off-by: NotWadeGrimridge <wade@grimridge.net>

--- Casks/pingplace.rb
+++ Casks/pingplace.rb
@@ -1,6 +1,6 @@
 cask "pingplace" do
-  version "1.4.1"
-  sha256 "91efd5ef72ed7ddbb4acef9c839724b041d13d6f942b55060c75e7d6a7ff44a8"
+  version "1.4.2"
+  sha256 "194ccc716627d7c266689f03c846b2779ac67e973b8718dc4c621e33dba88a11"
 
   url "https://github.com/NotWadeGrimridge/PingPlace/releases/download/v#{version}/PingPlace.app.tar.gz"
   name "PingPlace"
